6ef014ac6dcc4fb1b1690906dcf8f811e5f87a75
cdchushig <cdavid.chushig@gmail.com>
2020-05-15 22:13:11 +0200

    [cdchushig] Added controllers

---
+++ src/controllers/scratchproject.js
@@ -0,0 +1,60 @@
+import {getJsonProject} from '../scratchDownloader.js';
+
+class ScratchProjectController {
+
+  constructor(projectid) {
+    this.projectid = projectid;
+  }
+
+  async get(req, res) {
+    try {
+      let projectId = req.params.projectid;
+      const projectJson = await getJsonProject(projectId.toString());  
+      res.send(projectJson);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+
+// router.get('/api/v1/projects/:projectid', async(req, res, next) => {
+//     try{
+//         let projectId = req.params.projectid;
+
+//         getJsonProject(projectId.toString())
+//             .then((result) => {
+//                 return res.send(result)
+//             })
+//             .catch((err) => {
+//                 console.log(err);
+//         })
+    
+//     } catch(e) {
+//         res.status(500).send({message: 'Internal server error'})
+//     }
+// })
+
+    // async getById(req, res) {
+    //   const {
+    //     params: { id }
+    //   } = req;
+  
+    //   try {
+    //     const product = await this.Product.find({ _id: id });
+    //     res.send(product);
+    //   } catch (err) {
+    //     res.status(400).send(err.message);
+    //   }
+    // }
+    // async create(req, res) {
+    //   const product = new this.Product(req.body);
+    //   try {
+    //     await product.save();
+    //     res.status(201).send(product);
+    //   } catch (err) {
+    //     res.status(422).send(err.message);
+    //   }
+    // }
+  
+}
+  
+export default ScratchProjectController;
