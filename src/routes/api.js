973a5f18d32f70bc2d73bc3dc0336fa1649f17f2
cdchushig <cdavid.chushig@gmail.com>
2020-05-16 20:41:25 +0200

    [cdchushig] Update routes dividing api and other

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,6 +4,7 @@ import ScratchProjectController from '../controllers/scratchproject';
 const router = express.Router();
 const scratchprojectController = new ScratchProjectController();
 
-router.get('/:projectid', (req, res) => scratchprojectController.download(req, res));
+router.get('/', (req, res) => res.send('Bye!'));
+router.get('/:projectid', (req, res) => scratchprojectController.get(req, res));
 
 export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import apiRoute from './api';
 import scratchprojectRoute from './scratchproject';
 
 const router = express.Router();
 
-router.use('/api', scratchprojectRoute);
+router.use('/api', apiRoute);
+router.use('/', scratchprojectRoute);
 router.get('/', (req, res) => res.send('Hello World2!'));
 
 export default router;
